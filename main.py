020b3decd64ec87e7e4ff89cbc091c0a97525b78
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-12 20:02:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ st.header('Please upload a chest X-ray image')
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-model = load_model('./model/PMA.h5')
+model = load_model('./model/PMA2.h5')
 
 # load class names
 with open('./model/labels.txt', 'r') as f:
