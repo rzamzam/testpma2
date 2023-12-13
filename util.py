8fcbd73ab1cfd59e3b4c807c22a6d6dddc47da4d
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-13 23:12:27 +0700

    Update util.py

--- util.py
+++ util.py
@@ -14,10 +14,11 @@ from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 
-def classify_from_path(image_path, model, class_names):
-    # Load image from file path
-    image = Image.open(image_path).convert('RGB')
+from PIL import Image, ImageOps
+import numpy as np
+from tensorflow.keras.models import load_model
 
+def classify(image, model, class_names):
     # Resize image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
